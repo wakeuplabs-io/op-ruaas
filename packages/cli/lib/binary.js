582be744bb6cd4270d7957e527dc0c2123548f5f
matzapata <matuzapata@gmail.com>
2024-12-26 14:32:31 +0700

    release v0.1.2

--- packages/cli/lib/binary.js
+++ packages/cli/lib/binary.js
@@ -9,7 +9,7 @@ const error = msg => {
 
 // binary definition
 const repository = "https://github.com/wakeuplabs-io/op-ruaas"
-const tag_name = "v0.1.1"
+const tag_name = "v0.1.2"
 const name = "opruaas"
 
 const supportedPlatforms = [
