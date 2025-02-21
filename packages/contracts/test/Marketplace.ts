25d135d53f4d55be89fa9dde3e93ef10843af287
Matias Zapata <matuzapata@gmail.com>
2025-02-21 17:32:27 -0300

    deposit tests

--- packages/contracts/test/Marketplace.ts
+++ packages/contracts/test/Marketplace.ts
@@ -12,26 +12,66 @@ describe("Marketplace", function () {
 
     const Marketplace = await hre.ethers.getContractFactory("Marketplace");
     const marketplace = await Marketplace.deploy();
+    const marketplaceAddress = await marketplace.getAddress();
 
     const Token = await hre.ethers.getContractFactory("TestToken");
     const token = await Token.deploy(1000000n);
+    const tokenAddress = await token.getAddress();
 
     await marketplace.initialize(await token.getAddress());
 
-    return { marketplace, token, owner, otherAccount };
+    return {
+      marketplace,
+      marketplaceAddress,
+      token,
+      tokenAddress,
+      owner,
+      otherAccount,
+    };
   }
 
   describe("Initialize", function () {
     it("Should set the right payment token", async function () {
-      const { marketplace, token } = await loadFixture(deployMarketplaceFixture);
+      const { marketplace, token } = await loadFixture(
+        deployMarketplaceFixture
+      );
 
-      expect(await marketplace.paymentToken()).to.equal(await token.getAddress());
+      expect(await marketplace.paymentToken()).to.equal(
+        await token.getAddress()
+      );
     });
 
     it("Should fail if already initialized", async function () {
-      const { marketplace, token } = await loadFixture(deployMarketplaceFixture);
-      
-      await expect(marketplace.initialize(await token.getAddress())).to.be.reverted;
+      const { marketplace, token } = await loadFixture(
+        deployMarketplaceFixture
+      );
+
+      await expect(marketplace.initialize(await token.getAddress())).to.be
+        .reverted;
+    });
+  });
+
+  describe("Deposit", function () {
+    it("Should deposit tokens", async function () {
+      const { marketplace, marketplaceAddress, token, owner } =
+        await loadFixture(deployMarketplaceFixture);
+
+      await token.approve(marketplaceAddress, 10n);
+
+      expect(await marketplace.deposit(10n)).not.to.be.reverted;
+      expect(await token.balanceOf(marketplaceAddress)).to.equal(10n);
+      expect(await marketplace.deposits(owner.address)).to.equal(10n);
+    });
+
+    it("Should emit deposit event", async function () {
+      const { marketplace, marketplaceAddress, token, owner } =
+        await loadFixture(deployMarketplaceFixture);
+
+      await token.approve(marketplaceAddress, 10n);
+
+      expect(await marketplace.deposit(10n))
+        .to.emit(marketplace, "Deposit")
+        .withArgs(owner.address, 10n);
     });
   });
 
